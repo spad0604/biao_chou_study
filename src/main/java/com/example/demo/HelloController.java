bcb0297ccb319a275264599959e96f6fb0871af6
spad0604 <giap.nv225126@sis.hust.edu.vn>
2026-06-20 22:06:21 +0700

    first commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Chou!";
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "Greetings from Spring Boot!";
+    }
+}
